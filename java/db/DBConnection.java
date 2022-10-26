786036094cc3293f0c3dbe451dab4d86598edad0
mole9630 <1582616876@qq.com>
2022-10-26 21:18:45 +0800

    ✨新增连接数据库功能

---
+++ java/db/DBConnection.java
@@ -0,0 +1,50 @@
+package com.librarysys.db;
+
+import java.sql.*;
+
+public class DBConnection {
+    public static String strcom = "com.mysql.cj.jdbc.Driver";
+    public static String sqlUserName = "root";
+    public static String sqlUserPassword = "root";
+    public static String dbName = "library";
+    public static String sqlUrl = "jdbc:mysql://127.0.0.1:3306/" + dbName + "?user=" + sqlUserName + "&password=" + sqlUserPassword + "&useUnicode=true&characterEncoding=utf8";
+
+
+    public Connection getDBConnection() {
+            Connection con = null;
+        try {
+            Class.forName(strcom);
+            con = DriverManager.getConnection(sqlUrl);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return con;
+        }
+    }
+
+    public void closeDB(Connection con, PreparedStatement pstm, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstm != null) {
+                pstm.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 测试连接
+    public static void main(String[] args) {
+        DBConnection dbc = new DBConnection();
+        Connection con = null;
+        con = dbc.getDBConnection();
+        if (con != null){
+            System.out.println("连接成功!");
+        }
+    }
+}
